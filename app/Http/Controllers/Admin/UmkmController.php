86a46d87d74b64f654458d0ff4ad9299dc71065b
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-09-12 05:56:30 +0300

    feat: implement UmkmController and create index view for UMKM page

---
+++ app/Http/Controllers/Admin/UmkmController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UmkmController extends Controller
+{
+    public function index() {
+        return view('admin.umkm.index');
+
+    }
+}
